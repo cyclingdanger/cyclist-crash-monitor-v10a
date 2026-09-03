9ceadd0807418ba98d00dd92fe6c8bb566ce8004
cyclingdanger <chris_fix@hotmail.com>
2026-09-02 22:47:04 -0400

    Delete run_monitor.bat

--- run_monitor.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Installing/checking required packages...
-python -m pip install -r requirements.txt
-echo.
-echo Starting U.S. Cyclist Crash Monitor...
-python app.py
-pause
